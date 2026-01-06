1b73109a5426cdd374e78e8cd4ba43fbde8ad8fc
Mukul Gupta <mukulgupta@Mukuls-MacBook-Air.local>
2026-01-06 22:16:47 +0530

    Added code to listen on server

--- src/server.js
+++ src/server.js
@@ -45,8 +45,8 @@ app.use('/api/payments', require('./routes/paymentRoutes'));
 //     .then(() => console.log('MongoDB Connected'))
 //     .catch(err => console.error('MongoDB connection error:', err));
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
